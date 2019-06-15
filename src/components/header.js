d1d7baa1c3d45ea9bf140b6e0e636ca15e86682c
Luke Prosser <luke.prosser@cloud-iq.com>
2019-06-15 10:04:41 +0100

    Global styles, header design

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import headerStyles from '../styles/header.module.scss'
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header>
+    <div className={headerStyles.container}>
+      <h1 className={headerStyles.brand}>
+        <Link to="/">{siteTitle}</Link>
       </h1>
+      <nav>
+        <Link className={headerStyles.navItem} activeClassName={headerStyles.active} to="/">Work</Link>
+        <Link className={headerStyles.navItem} activeClassName={headerStyles.active} to="/about">About</Link>
+      </nav>
     </div>
   </header>
 )
