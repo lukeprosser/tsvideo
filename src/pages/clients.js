4a4ce34468afc590642d3e3539790eeae6b2e637
Luke Prosser <luke.prosser@cloud-iq.com>
2019-06-26 06:29:32 +0100

    Header mobile fixes, client grid layout

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -15,27 +15,24 @@ const ClientPage = (props) => (
     <div className={clientStyles.container}>
       <div className={clientStyles.testimonials}>
 
-        <div className={clientStyles.clientCard}>
-          <div className={clientStyles.client}>
+          <div className={clientStyles.client1}>
             <FontAwesomeIcon icon={faDog} className={clientStyles.clientLogo} />
-            <p>Client Name</p>
+            <p className={clientStyles.clientName}>Client Name</p>
           </div>
-          <p className={clientStyles.blurb}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
-        </div>
-        <div className={clientStyles.clientCard}>
-          <div className={clientStyles.client}>
+          <p className={clientStyles.blurb1}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
+        
+          <div className={clientStyles.client2}>
             <FontAwesomeIcon icon={faDog} className={clientStyles.clientLogo} />
-            <p>Client Name</p>
+            <p className={clientStyles.clientName}>Client Name</p>
           </div>
-          <p className={clientStyles.blurb}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
-        </div>
-        <div className={clientStyles.clientCard}>
-          <div className={clientStyles.client}>
+          <p className={clientStyles.blurb2}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
+        
+          <div className={clientStyles.client3}>
             <FontAwesomeIcon icon={faDog} className={clientStyles.clientLogo} />
-            <p>Client Name</p>
+            <p className={clientStyles.clientName}>Client Name</p>
           </div>
-          <p className={clientStyles.blurb}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
-        </div>
+          <p className={clientStyles.blurb3}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
+        
       </div>
 
     </div>
--- src/pages/clients_temp.js
+++ src/pages/clients_temp.js
@@ -15,24 +15,27 @@ const ClientPage = (props) => (
     <div className={clientStyles.container}>
       <div className={clientStyles.testimonials}>
 
-          <div className={clientStyles.client1}>
+        <div className={clientStyles.clientCard}>
+          <div className={clientStyles.client}>
             <FontAwesomeIcon icon={faDog} className={clientStyles.clientLogo} />
             <p>Client Name</p>
           </div>
-          <p className={clientStyles.blurb1}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
-        
-          <div className={clientStyles.client2}>
+          <p className={clientStyles.blurb}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
+        </div>
+        <div className={clientStyles.clientCard}>
+          <div className={clientStyles.client}>
             <FontAwesomeIcon icon={faDog} className={clientStyles.clientLogo} />
             <p>Client Name</p>
           </div>
-          <p className={clientStyles.blurb2}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
-        
-          <div className={clientStyles.client3}>
+          <p className={clientStyles.blurb}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
+        </div>
+        <div className={clientStyles.clientCard}>
+          <div className={clientStyles.client}>
             <FontAwesomeIcon icon={faDog} className={clientStyles.clientLogo} />
             <p>Client Name</p>
           </div>
-          <p className={clientStyles.blurb3}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
-        
+          <p className={clientStyles.blurb}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio praesentium molestias fugiat eligendi doloremque reiciendis vero magni dolorem. Earum deleniti vitae inventore voluptatem cumque. Commodi quasi necessitatibus quod earum neque!"</p>
+        </div>
       </div>
 
     </div>
