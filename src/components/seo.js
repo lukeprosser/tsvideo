b9d190b15a541ebb6a6d6fb66d41fc1a2b748299
Luke Prosser <luke.prosser@cloud-iq.com>
2019-10-01 09:37:54 +0100

    hard code og image ref

--- src/components/seo.js
+++ src/components/seo.js
@@ -27,7 +27,8 @@ const SEO = ({ title, description, image, pathname, article }) => (
         <>
           <Helmet title={seo.title} titleTemplate={titleTemplate}>
             <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.image} />
+            {/* <meta name="image" content={seo.image} /> */}
+            <meta name="image" content="https://tomsmithvideo.co.uk/og_image.jpg" />
             {seo.url && <meta property="og:url" content={seo.url} />}
             {(article ? true : null) && (
               <meta property="og:type" content="article" />
@@ -36,13 +37,15 @@ const SEO = ({ title, description, image, pathname, article }) => (
             {seo.description && (
               <meta property="og:description" content={seo.description} />
             )}
-            {seo.image && <meta property="og:image" content={seo.image} />}
+            {/* {seo.image && <meta property="og:image" content={seo.image} />} */}
+            {seo.image && <meta property="og:image" content="https://tomsmithvideo.co.uk/og_image.jpg" />}
             <meta name="twitter:card" content="summary_large_image" />
             {seo.title && <meta name="twitter:title" content={seo.title} />}
             {seo.description && (
               <meta name="twitter:description" content={seo.description} />
             )}
-            {seo.image && <meta name="twitter:image" content={seo.image} />}
+            {/* {seo.image && <meta name="twitter:image" content={seo.image} />} */}
+            {seo.image && <meta name="twitter:image" content="https://tomsmithvideo.co.uk/og_image.jpg" />}
           </Helmet>
         </>
       )
